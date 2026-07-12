d9e2989b931a327cadab8406eccce8d8a5d7232b
Martin Maldonado <tin.maldonado@gmail.com>
2026-07-12 19:21:38 -0300

    chore(mobile): configure eslint, prettier and path alias

---
+++ mobile/eslint.config.js
@@ -0,0 +1,5 @@
+const { defineConfig } = require("eslint/config");
+const expo = require("eslint-config-expo/flat");
+const prettier = require("eslint-plugin-prettier/recommended");
+
+module.exports = defineConfig([expo, prettier, { ignores: ["dist/*"] }]);
